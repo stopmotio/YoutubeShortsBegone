9cf7618ee0de61c602d92191ca47151bc36686b5
stopmotio <homeyone2387@gmail.com>
2021-06-03 19:40:11 -0400

    create

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function FixTheLink() {
+  var workvar = document.getElementById('sourceurl').value;
+  workvar = workvar.replace('shorts/', 'watch?v=');
+  workvar = workvar.replace('?feature=share', ''); //Clean up final URL. If copied link from SHARE button will add this extra data
+
+  //Now all we have to do is output this
+  console.log('Parsing completed with a final URL of: ' + workvar);
+  document.getElementById('output').href = workvar;
+  document.getElementById('output').innerHTML = workvar;
+}
