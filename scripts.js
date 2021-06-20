142945e187addde819104b710e88544bd0d9b142
stopmotio <homeyone2387@gmail.com>
2021-06-20 19:38:58 -0400

    Pretty Pretty Pretty

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 function FixTheLink() {
   var workvar = document.getElementById('sourceurl').value;
   workvar = workvar.replace('shorts/', 'watch?v=');
-  workvar = workvar.replace('?feature=share', ''); //Clean up final URL. If copied link from SHARE button will add this extra data
+  workvar = workvar.replace('?feature=share', ''); //Clean up final URL. If copied link from SHARE button will add this extra data, not sure how the servers handle this.
 
   //Now all we have to do is output this
   console.log('Parsing completed with a final URL of: ' + workvar);
   document.getElementById('output').href = workvar;
   document.getElementById('output').innerHTML = workvar;
-
-  //and somehow this is still majority HTML according to GitHub/Linguist. Thanks!
 }
